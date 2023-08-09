d45480a06ea0071d337fe7cda7cb5dd65091bf43
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-08-09 17:06:26 -0300

    feat: create a home page, add dep and create a initial structure

---
+++ routes/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+// Styles
+import { styles } from "../utils/styles";
+
+export function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Home page!</Text>
+        </View>
+    );
+}
