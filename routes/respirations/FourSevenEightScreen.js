e847c53170268301f9a159a99eff2b203713bf8e
luizolivas <luizolivas@gmail.com>
2023-08-28 01:06:50 -0300

    temporizador respiracao 478

--- routes/respirations/FourSevenEightScreen.js
+++ routes/respirations/FourSevenEightScreen.js
@@ -1,1 +1,106 @@
-export function FourSevenEightScreen() {}
+import React, { useState, useEffect } from 'react';
+import { Text, TouchableOpacity, StyleSheet, SafeAreaView, View } from 'react-native';
+
+import { BackButton } from "../../components/BackButton/BackButton";
+
+export function FourSevenEightScreen() {
+    const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [circleColor, setCircleColor] = useState('lightblue'); 
+    const [counterCicles, setCounterCicles] = useState(0);
+
+    useEffect(() => {
+        let interval;
+
+        if (isStopwatchStart) {
+            interval = setInterval(() => {
+                setCurrentTime((prevTime) => prevTime + 1);
+            }, 1000);
+            if (currentTime === 5 && circleColor == 'lightblue') {
+                setCurrentTime(1)
+                setCircleColor('green');
+                
+            }
+            else if( currentTime === 8 && circleColor == 'green'){
+                setCurrentTime(1)
+                setCircleColor('red');
+            }
+            else if( currentTime === 9 && circleColor == 'red'){
+                setCurrentTime(0)
+                setCounterCicles(counterCicles + 1)
+                setCircleColor('lightblue');
+            }
+        } else {
+            clearInterval(interval);
+        }
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isStopwatchStart, currentTime]);
+
+    return (
+        
+            <View style={styles.container}>
+                <BackButton></BackButton>
+                <Text style={styles.title}>Ciclos de respirações realizados: {counterCicles}</Text>
+                <View style={[styles.circle, { backgroundColor: circleColor }]}>
+                    <Text style={styles.circleText}>{currentTime}</Text>
+                </View>
+                <TouchableOpacity
+                    onPress={() => {
+                        setIsStopwatchStart(!isStopwatchStart);
+                        setCircleColor('lightblue');
+                    }}>
+                    <Text style={styles.buttonText}>
+                        {!isStopwatchStart ? 'INICIAR' : 'PARAR'}
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => {
+                        setIsStopwatchStart(false);
+                        setCurrentTime(0);
+                    }}>
+                    <Text style={styles.buttonText}>REINICIAR</Text>
+                </TouchableOpacity>
+            </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 25,
+        fontWeight: 'bold',
+        padding: 20,
+    },
+    secondsText: {
+        fontSize: 40,
+        marginBottom: 20,
+    },
+    buttonText: {
+        fontSize: 20,
+        marginTop: 10,
+    },
+    circleText: {
+        fontSize: 100,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    circle: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        backgroundColor: 'lightblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
