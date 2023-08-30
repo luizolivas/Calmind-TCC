23a65db019229fba0a251fca1af121ee4f91d072
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-08-30 09:46:02 -0300

    add title in help contents

--- components/HelpContentElement/HelpContentElement.js
+++ components/HelpContentElement/HelpContentElement.js
@@ -7,13 +7,14 @@ import { useNavigation } from "@react-navigation/native";
 // Icons in https://fontawesome.com/v4/icons/
 import Icon from "react-native-vector-icons/FontAwesome";
 
-export function HelpContentElement({ name, color, navigationRoute, text }) {
+export function HelpContentElement({ name, color, title, navigationRoute, text }) {
 
     const navigation = useNavigation();
 
     return (
         <TouchableOpacity style={stylesHelpContent.contentView} onPress={() => navigation.navigate(navigationRoute)} >
             <Icon style={[styles.icon, { flex: 1 }]} name={name} size={65} color={color} />
+            <Text style={stylesHelpContent.contentTitle}>{title}</Text>
             <Text style={stylesHelpContent.contentDescription}>{text}</Text>
         </TouchableOpacity>
     );
@@ -28,6 +29,12 @@ const stylesHelpContent = StyleSheet.create({
         marginBottom: 30,
         paddingBottom: 10,
         borderBottomWidth: 1
+    },
+    contentTitle: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginTop: 5
     },
     contentDescription: {
         flex: 1,
