164a40649a75de27f6360a768b58ebbf8396013e
luizolivas <luizolivas@gmail.com>
2023-09-10 20:19:23 -0300

    Instruction Pranayama Respiration OK

--- routes/respirations/Pranayama/InfoPranayamaScreen.js
+++ routes/respirations/Pranayama/InfoPranayamaScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, Linking } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image, Linking } from "react-native";
 import { useState } from 'react';
 
 import { styles } from "../../../utils/styles";
@@ -8,6 +8,9 @@ import { useNavigation } from "@react-navigation/native";
 // Icons in https://fontawesome.com/v4/icons/
 import Icon from "react-native-vector-icons/FontAwesome";
 
+import hand from "../../../assets/hand.jpg"
+import stepOne from "../../../assets/stepOne.jpg"
+
 
 
 // Components
@@ -16,6 +19,8 @@ import { Button } from "../../../components/Button/Button";
 
 export function InfoPranayamaScreen() {
     const navigation = useNavigation();
+
+
 
     const navigateRespirate = () => {
         navigation.navigate('Pranayama');
@@ -29,25 +34,33 @@ export function InfoPranayamaScreen() {
     return (
         <View style={styles.container}>
             <BackButton />
-            <Text style={styles.title}>Respiração das Narinas Alternadas (Nadi Shodhan Pranayama)</Text>
-            <Text style={styles.description}>
-                A respiração das narinas alternadas é uma técnica de respiração que visa equilibrar a energia do corpo, acalmar a mente e melhorar a concentração.
-            </Text>
-            <View>
-                <Image
-                    style={{ width: 100, height: 100 }} // Defina os valores de largura e altura desejados
+                <ScrollView showsVerticalScrollIndicator={false}>
+                <Text style={styles.title}>Respiração das Narinas Alternadas (Nadi Shodhan Pranayama)</Text>
+                <Text style={styles.description}>
+                    A respiração das narinas alternadas é uma técnica de respiração que visa equilibrar a energia do corpo, acalmar a mente e melhorar a concentração.
+                </Text>
+                <Text style={stylesinfoPranayama.instruction}>COMO FAZER?</Text>
+                <View style={stylesinfoPranayama.container}>
+                    
 
-                />
-            </View>
-            <View>
-                <TouchableOpacity onPress={handleLinkPress}>
-                    <Text style={stylesinfoPranayama.linkText}>Preciso de ajuda com essa respiração</Text>
-                </TouchableOpacity>
-            </View>
+                    <Image source={stepOne} style={{ width: 250, height: 150 }} />
+                    <View style={stylesinfoPranayama.innerCard}> 
+                        <Text style={stylesinfoPranayama.text}>Para começar, verifique a numeração dos dedos na imagem abaixo e posicione em você conforme imagem acima, assim que estiver pronto clique em "Iniciar".</Text>
+                        <Image source={hand} style={{ width: 200, height: 200 }} />
+                    </View>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={handleLinkPress}>
+                        <Text style={stylesinfoPranayama.linkText}>Preciso de ajuda com essa respiração</Text>
+                    </TouchableOpacity>
+                </View>
 
-            <Button text="Iniciar" onPress={navigateRespirate} style={stylesinfoPranayama.button} />
-            <View style={{ flex: 1 }}></View>
+                <Button text="Iniciar" onPress={navigateRespirate} style={stylesinfoPranayama.button} />
+                <View style={{ flex: 1 }}></View>
+            </ScrollView>
         </View>
+        
+        
     );
 }
 
@@ -58,16 +71,43 @@ const stylesinfoPranayama = StyleSheet.create({
         alignItems: 'center'
     },
     button: {
-        width: 260,
+        width: 300,
         height: 60,
         marginTop: 50,
         backgroundColor: "#50C878"
     },
-    imagee: {
-
-    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 1, 
+      },
+      text: {
+        textAlign: 'justify',
+        width: 250,
+      },
     linkText: {
         color: 'blue',
         textDecorationLine: 'underline',
+        textAlign: 'center',
+        marginTop: 10,
     },
+    innerCard: {
+        borderRadius: 10,
+
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      instruction: {
+        fontSize: 18,
+        fontWeight: 'bold', 
+        backgroundColor: '#50C878',
+        color: 'white', 
+        padding: 8, 
+        borderTopLeftRadius: 10, 
+        borderTopRightRadius: 10, 
+        textAlign: 'center',
+      },
 });
