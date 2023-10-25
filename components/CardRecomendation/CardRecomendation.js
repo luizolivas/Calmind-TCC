0514c1aedfd18f3c6e90c597f7ba71db45131c52
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-10-25 11:47:35 -0300

    fix: card recommendation in IOS and include owner in this project

--- components/CardRecomendation/CardRecomendation.js
+++ components/CardRecomendation/CardRecomendation.js
@@ -1,9 +1,11 @@
-import { Text, Dimensions } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 
 const isTablet = Dimensions.get('window').width >= 600;
 
 export function CardRecomendation({ text, bgColor }) {
     return (
-        <Text style={{ fontSize: isTablet ? 18 : 14, backgroundColor: bgColor, borderColor: 'black', borderRadius: 8, flex: 1, textAlign: 'center', textAlignVertical: 'center', color: 'white', fontFamily: 'Roboto_700Bold' }}>{text}</Text>
+        <View style={{ fontSize: isTablet ? 18 : 14, backgroundColor: bgColor, borderColor: 'black', borderRadius: 8, flex: 1, textAlign: 'center', textAlignVertical: 'center', color: 'white', fontFamily: 'Roboto_700Bold' }}>
+            <Text style={{ fontSize: isTablet ? 18 : 14, backgroundColor: bgColor, borderColor: 'black', borderRadius: 8, flex: 1, textAlign: 'center', textAlignVertical: 'center', color: 'white', fontFamily: 'Roboto_700Bold' }}>{text}</Text>
+        </View>
     );
 }
